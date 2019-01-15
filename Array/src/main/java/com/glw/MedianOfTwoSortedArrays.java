7f25ed3d2bc62c8bc2f35d1a28b1903e6bbb7959
aoc <510537547@qq.com>
2019-01-15 11:02:45 +0800

    更新：MedianOfTwoSortedArrays题目描述

--- Array/src/main/java/com/glw/MedianOfTwoSortedArrays.java
+++ Array/src/main/java/com/glw/MedianOfTwoSortedArrays.java
@@ -1,17 +1,32 @@
 package com.glw;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author glw
  * @date 2019/1/14 9:10
+ * 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
+ * 请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+ * 你可以假设 nums1 和 nums2 不会同时为空。
+ *
+ * 示例 1:
+ * nums1 = [1, 3]
+ * nums2 = [2]
+ * 则中位数是 2.0
+ *
+ * 示例 2:
+ * nums1 = [1, 2]
+ * nums2 = [3, 4]
+ * 则中位数是 (2 + 3)/2 = 2.5
  */
 public class MedianOfTwoSortedArrays {
 //    public static double findMedianSortednums1rrays(int[] nums1, int[] nums2) {
 //        int index = nums1.length + nums2.length;
 //        int[] nums = new int[index];
 //        int i = 0;
-//        List temp = new nums1rrayList();
+//        List temp = new ArrayList();
 //        do{
 //            if (nums1[i] < nums2[index - i - 1]) {
 //                nums[i] = nums1[i];
