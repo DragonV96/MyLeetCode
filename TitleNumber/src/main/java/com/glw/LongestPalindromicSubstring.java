d8c7ed83b696d05a043fbc1e0c2c3da606a73d01
aoc <510537547@qq.com>
2019-02-12 17:29:36 +0800

    每日一题 5. Longest Palindromic Substring(官方答案)

---
+++ TitleNumber/src/main/java/com/glw/LongestPalindromicSubstring.java
@@ -0,0 +1,48 @@
+package com.glw;
+
+/**
+ * @author glw
+ * @date 2019/2/2 10:06
+ * 5. Longest Palindromic Substring
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ *
+ * 示例 1：
+ * 输入: "babad"
+ * 输出: "bab"
+ * 注意: "aba" 也是一个有效答案。
+ *
+ * 示例 2：
+ * 输入: "cbbd"
+ * 输出: "bb"
+ */
+public class LongestPalindromicSubstring {
+    public static String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private static int expandAroundCenter(String s, int left, int right) {
+        int l = left, r = right;
+        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+            l--;
+            r++;
+        }
+        return r - l - 1;
+    }
+
+    public static void main(String[] args) {
+        String input = "dsafsgaagsss";
+        String result = longestPalindrome(input);
+        System.out.println("官方答案：   输入字符串：" + input + "\n结果：" + result);
+    }
+}
